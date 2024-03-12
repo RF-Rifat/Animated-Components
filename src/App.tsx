955a5ea9aa037c35495503a26613ac7f56563390
RF-Rifat <rfrifat6344@gmail.com>
2024-03-12 18:56:12 +0600

    making all route responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [click, setClick] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="w-screen h-screen bg-zinc-100 p-2 text-zinc-700 items-center justify-center grid">
+        <div>
+          <button
+            onClick={() => {
+              setClick(!click);
+            }}
+            className="group transition-all duration-200 p-2 flex flex-row items-center justify-center bg-white gap-2 rounded-lg border border-zinc-200"
+          >
+            <span>Dropdown</span>
+          </button>
+          <AnimatePresence>
+            {click && (
+              <motion.div
+                initial={{
+                  scale: 1,
+                  y: 0,
+                }}
+                animate={{
+                  scale: 0,
+                  y: 10,
+                }}
+                exit={{
+                  scale: 1,
+                  y: 0,
+                }}
+                transition={{
+                  duration: 1,
+                }}
+                className="shadow-lg left-0 w-full h-max p-2 bg-white border border-zinc-200 rounded-lg flex flex-col gap-2"
+              >
+                <span className="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg">
+                  <p>Light</p>
+                </span>
+                <span className="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg">
+                  <p>Dark</p>
+                </span>
+                <span className="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg">
+                  <p>System</p>
+                </span>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
